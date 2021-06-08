02e7ae44b30dc5c2d02c465eb9ee72e7cb95f549
Indhu R <64062602+IndhuR@users.noreply.github.com>
2021-06-08 10:16:52 +0530

    Update stopwatch.c

--- stopwatch.c
+++ stopwatch.c
@@ -1,97 +1,67 @@
-#include<conio.h>
-#include<process.h>
 #include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+#include<time.h>
 #include<windows.h>
-int h=0,m=0,s=0;
-char ch='p';
+main()
+{
+int choice, h,m,s; h=0; m=0; s=0; //--variable declaration--//
+char p= 'p',e='e';
+printf("\n\n\n\n\n\n\n\t\t\t\tPROJECT BY INDHU \n\n\n\n\n\t\t\t\t press any key to enter");
+getch();
 
-void main()
-{
-void watch();
-watch();
-
-while(1)
-{
-if(kbhit())
-ch=getch();
-if(ch=='s'||ch=='S')
-break;
-if(ch=='e'||ch=='E')
-exit(0);
-}
-
-
-while(1)
-{
-watch();
-sleep(1);
-
-if(kbhit())
-ch=getch();
-
-if(ch=='r'||ch=='R')
-{
-h=m=s=0;
-watch();
-
-while(1)
-{
-if(kbhit())
-ch=getch();
-if(ch=='s'||ch=='S')
-break;
-if(ch=='e'||ch=='E')
-exit(0);
-}
-}
-else
-if(ch=='p'||ch=='P')
-while(1)
-{
-if(kbhit())
-ch=getch();
-if(ch=='s'||ch=='S')
-break;
-if(ch=='e'||ch=='E')
-exit(0);
-if(ch=='r'||ch=='R')
-{
-ch='c';
-h=m=s=0;
-watch();
-}
-}
-else
-if(ch=='e'||ch=='E')
-exit(0);
-
-if(s!=59)
-s++;
-else
-{
-s=0;
-if(m!=59)
-m++;
-else
-{
-m=0;
-h++;
-}
-}
-}
-}
-
-void watch()
-{
 system("cls");
-printf("\n\n\t\t\t\t\tStopwatch \n\n\t\t\t\t\t");
-
-printf("%02d:%02d:%02d",h,m,s);
-
-printf("\n\n\n\t\t\t\t\tPress Key");
-printf("\n\t\t\t\t\t---------------");
-printf("\n\n\n\t\t\t\t\ts -> Start");
-printf("\n\n\n\t\t\t\t\tp -> Pause");
-printf("\n\n\n\t\t\t\t\tr -> Reset");
-printf("\n\n\n\t\t\t\t\te -> Exit");
+printf("Press 1 to start the timer\nPress 2 to exit\n");
+printf("\nEnter your choice\n");
+scanf("%d",&choice);
+switch(choice) //--switch case --//
+{
+case 1:
+{
+while(1) //--while condition is true//
+{
+if(s>59) //--if seconds(s) is > 59--//
+{
+m=m+1; //--increment minute by 1--//
+s=0;
+}
+if(m>59) //--if minutes(s) is > 59--//
+{
+h=h+1; //--increment hour by 1--//
+m=0;
+}
+if(h>11) //--if hour(h) is > 11--//
+{
+h=0; //-Hour to 0--//
+m=0;
+s=0;
+}
+Sleep(1000); //--inbuilt function for 1sec delay--//
+s=s+1;
+system("cls"); //--Clear screen--//
+printf("\n\n\n\n\t\t\t\t\t\tDIGITAL CLOCK");
+printf("\n\n\n\n\n\t\t\tHOUR:MINUTE:SECOND");
+printf("\n\n\t\t\t%02d  :  %02d  :  %02d",h,m,s); //--Print time--//
+printf("\n\n\n\n\n\n\nTo pause : press P\nTo pause and exit :press E\nTo resume : press c\n");
+if(kbhit()) //--Check if any button is pressed on keyboard--//
+{
+if(e==getch())
+ exit(0);
+if(p==getch()) //--Check if P is pressed--//
+{
+system("pause"); //--Inbuilt function for pause and resume--//
+}
+}
+}
+break;
+}
+case 2:
+exit(0); //--Exit --//
+default:
+{
+printf("Wrong Choice");
+}
+}
+getch(); //--Holding the screen--//
+return 0;
 }
